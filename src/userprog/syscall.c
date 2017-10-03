a2502864fffdeb4337176eeb8c14fcb0caa95cbf
Reminiel <reminiel@naver.com>
2017-10-03 14:33:19 +0900

    Syscall implementation & local fs.dsk

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -4,6 +4,10 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
+//System call functions
+static void sys_halt (void);
+
+//System call handler
 static void syscall_handler (struct intr_frame *);
 
 void
@@ -12,9 +16,47 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-static void
-syscall_handler (struct intr_frame *f UNUSED) 
+
+syscall_handler (struct intr_frame *f)
 {
   printf ("system call!\n");
-  thread_exit ();
+  //Implement here
+  int i = 0, args[4];
+  while(i < 4)
+    {
+      args[i] = ((int *)f->esp + i);
+      i++;
+    }
+  switch(args[0])
+    {
+      case SYS_HALT:
+        sys_halt();
+        break;
+      case SYS_EXEC:
+        break;
+      case SYS_WAIT:
+        break;
+      case SYS_CREATE:
+        break;
+      case SYS_REMOVE:
+        break;
+      case SYS_OPEN:
+        break;
+      case SYS_FILESIZE:
+        break;
+      case SYS_READ:
+        break;
+      case SYS_WRITE:
+        break;
+      case SYS_SEEK:
+        break;
+      case SYS_TELL:
+        break;
+      case SYS_CLOSE:
+        break;
+    }
 }
+void sys_halt(void)
+  {
+    shutdown_power_off();
+  }
